03080bb01487a296afaa74ff1488130b432db427
ntdung6868 <ntdungqlqb@gmail.com>
2025-06-09 18:12:06 +0700

    Install and configure shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-red-400 w-screen h-screen flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold underline text-center p-10">Hello Dũng nè!</h1>
-    </>
+      <Button type="primary" shape="circle">
+        Click me!
+      </Button>
+    </div>
   );
 }
 
